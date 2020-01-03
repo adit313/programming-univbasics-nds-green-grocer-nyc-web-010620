645fb40724f3697d6a0ce4fd6c3043fce735f2fd
Adit P Patel <app2116@gmail.com>
2020-01-03 17:38:49 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -35,6 +35,8 @@ def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  
+  return_cart
 end
 
 def apply_clearance(cart)
