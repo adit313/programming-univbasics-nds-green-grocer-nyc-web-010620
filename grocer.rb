b94f409d636192770da93dd936864a087ed04eeb
Adit P Patel <app2116@gmail.com>
2020-01-03 17:55:32 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -48,7 +48,7 @@ def apply_coupons(cart, coupons)
         :item => "#{cart[i][:item]} W/COUPON",
         :price => coupons[j][:price]/coupons[j][:num],
         :clearance => cart[i][:clearance],
-        :count => 
+        :count => coupons[j][:num]
       }
     end
       i+=1
