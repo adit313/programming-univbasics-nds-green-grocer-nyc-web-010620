53aff592f39b6a2006fe321afb10dfbb0c9ec277
Adit P Patel <app2116@gmail.com>
2020-01-03 17:06:42 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -26,7 +26,7 @@ def consolidate_cart(cart)
    result << {
      :item => cart[i][:item], 
      :price => cart[i][:price], 
-     :clearence => if cart[i][:clearence] == "true" then true else false end, 
+     :clearence => cart[i][:clearence], 
      :count => allitems.count(cart[i][:item])
    }
    i += 1
